3e8fedbe3aa63a3996a1953e41ca265a6656a2fc
Khalentello <nicofelipe26@hotmail.com>
2022-10-28 11:34:59 -0500

    primer commit

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+const path = required("path");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "main.js",
+  },
+  resolve: {
+    extensions: [".js"],
+  },
+};
