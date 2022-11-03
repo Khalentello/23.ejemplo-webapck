d57fa529c806b9d1c28a1762e418fe93d7a0f223
Khalentello <nicofelipe26@hotmail.com>
2022-11-02 19:11:32 -0500

    solucion de las comillas francesas en script

--- script/create-env.js
+++ script/create-env.js
@@ -1,3 +1,3 @@
 const fs = require("fs"); //file system, modulo de node que permite trabajar con el sistema operativo para guardar o enviar datos
 
-fs.writeFileSync("./.env", "API= ${process.env.API}\n");
+fs.writeFileSync("./.env", `API= ${process.env.API}\n`);
